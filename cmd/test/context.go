e0f4577f05b554c74d6a0463243b67be9a510044
kitamin <11195207+meian@users.noreply.github.com>
2020-04-17 19:31:31 +0900

    Revert "#29 異なるパッケージを監視対象にする処理を追加"

--- cmd/test/context.go
+++ cmd/test/context.go
@@ -16,8 +16,8 @@ const (
 
 // Context はテスト実行時の情報を保持する
 type Context struct {
+	Config      *Config
 	Watcher     *notify.Watcher
-	Args        []string
 	Directories []string
 	Changed     *file.PairMap
 	State       int
@@ -27,33 +27,20 @@ type Context struct {
 
 // NewContext はコンフィグから実行情報を生成する
 func NewContext(config *Config) (*Context, error) {
-	dirs, err := collectDirs(config)
-	if err != nil {
-		return nil, err
+	c := &Context{
+		Config:  config,
+		Changed: file.NewPairMap(),
+		State:   None,
+		Done:    make(chan error),
 	}
-	return &Context{
-		Changed:     file.NewPairMap(),
-		State:       None,
-		Done:        make(chan error),
-		Args:        config.Args,
-		Directories: dirs,
-	}, nil
-}
-
-func collectDirs(config *Config) ([]string, error) {
-	dirs := []string{}
-	dirMap := map[string]bool{}
-	for _, dir := range config.Dirs {
-		tdirs, err := file.TargetDirs(dir, config.Recursive)
+	if config.Recursive {
+		dirs, err := file.RecurseDir(config.Dir)
 		if err != nil {
 			return nil, err
 		}
-		for _, tdir := range tdirs {
-			if _, ok := dirMap[tdir]; !ok {
-				dirs = append(dirs, tdir)
-				dirMap[tdir] = true
-			}
-		}
+		c.Directories = dirs
+	} else {
+		c.Directories = []string{config.Dir}
 	}
-	return dirs, nil
+	return c, nil
 }
